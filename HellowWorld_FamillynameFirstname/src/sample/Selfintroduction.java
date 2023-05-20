231e32eab16a478540129fadde8dca7ef97b519a
reo-s1213 <133978237+reo-s1213@users.noreply.github.com>
2023-05-21 00:43:40 +0900

    １

---
+++ HellowWorld_FamillynameFirstname/src/sample/Selfintroduction.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class Selfintroduction {
+
+	public static void main(String[] args) {
+		String name = "鈴木　麗生";
+		int age = 21;
+		String hobby = "筋トレ";
+		
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
