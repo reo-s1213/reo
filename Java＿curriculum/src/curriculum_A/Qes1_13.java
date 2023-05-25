3921fb277d25a37afbc35de16faf11916874bc70
reo-s1213 <133978237+reo-s1213@users.noreply.github.com>
2023-05-25 18:22:03 +0900

    修正１

--- Java＿curriculum/src/curriculum_A/Qes1_13.java
+++ Java＿curriculum/src/curriculum_A/Qes1_13.java
@@ -5,7 +5,11 @@ public class Qes1_13 {
 	public static void main(String[] args) {
 		
 		/*
-		 * Qes1
+		 * Qes1　下記9個をローカル変数として宣言のみしてください
+		 * ・バイト型・短整数型・整数型・長整数型
+		 * ・単精度浮動小数点数型・倍精度浮動小数点数型
+		 * ・文字型・文字列型
+		 * ・ブーリアン型
 		 */
 		
 		//バイト型
@@ -28,7 +32,7 @@ public class Qes1_13 {
 		boolean data;
 		
 		/*
-		 * Qes2
+		 * Qes2　それぞれのローカル変数をローカル内でそれぞれの初期値を代入し初期化してください
 		 */
 		
 		//それぞれの変数を初期化する
@@ -38,19 +42,19 @@ public class Qes1_13 {
 		d = 0L;
 		e =  0.0f;
 		f = 0.0d;
-		ch = 0;
+		ch = 0000;
 		str = null;
 		data = false;
 		
 		/*
-		 * Qes3
+		 * Qes3 初期化をしたそれぞれの変数に下記の値を代入してください
 		 */
 		
 		//それぞれの変数に代入する
 		num = 10;
 		b = 100;
 		c = 1000;
-		d = 10000;
+		d = 10000L;
 		e = 9.5f;
 		f = 10.5d;
 		ch = 'a';
@@ -58,33 +62,39 @@ public class Qes1_13 {
 		data = true;
 		
 		/*
-		 * Qes4
+		 * Qes4 下記の通りにコンソール出力されるようにしてください
+		 *      上記で作成した変数を必ず使用すること
 		 */
 		
 		//11110を出力
-		System.out.print(num + b + c + d);
 		System.out.println(num + b + c + d);
 		//20を出力
-		System.out.print(num * 2);
 		System.out.println(num * 2);
 		//a ハロー trueを出力
-		System.out.print(ch + str + data);
 		System.out.println(ch + str + data);
 		//11130を出力
-		System.out.print(num + b + c + d + e + f);
 		System.out.println(num + b + c + d + e + f);
 		//小数点以外の数字を全てかける
-		System.out.print(num * b * c * d);
 		System.out.println(num * b * c * d);
 		//10.5割る100をする
-		System.out.print(f / b);
 		System.out.println(f / b);
 		//10引く100をする
-		System.out.print(num - b);
 		System.out.println(num - b);
 		
 		/*
-		 * Qes5
+		 * Qes5  次のプログラムを実行すると「ハローJAVA2023」という結果が表示されます。
+		 *      「ハローJAVA43」と表示とさせたいのですが、意図通りに動きません。正しく動作するように修正してください。
+		 */
+		
+		//「ハローJAVA43」と出力する
+		int number=20;
+		int number1=23;
+		System.out.println("ハローJAVA" + (number + number1));
+		
+		/*
+		 * Qes6 『』で囲われた人の情報を変数にして、formatの通りコンソールに出力してください
+		 *       ローカル変数に代入し○○に入れてください
+		 *      『山田太郎 18歳 170.5cm 62.2kg 寿司』
 		 */
 		
 		//『山田太郎 18歳 170.5cm 62.2kg 寿司』を出力
@@ -100,14 +110,18 @@ public class Qes1_13 {
 		System.out.println("好きな食べ物は"+str+"です");
 		
 		/*
-		 * Qes6
+		 * Qes7 6で作成した自己紹介に続いてBMIが出力されるようにしてください
+		 * 「BMIは○○です」
+		 * ただし計算は数値を直書きせず、全て変数を使ってすること
 		 */
 		
 		//BMIをs出力
-		System.out.println("BMIは"+e/(f*f)*10000+"です");
+		double bm = e / (f*f) * 10000;
+		String bmi = String.format("%.1f", bm);
+		System.out.println("BMIは" + bmi + "です");
 		
 		/*
-		 * Qes8
+		 * Qes8 6で宣言した変数に再代入し下記の通りコンソールに出力してください
 		 */
 		
 		//コンソールに出力
@@ -121,10 +135,12 @@ public class Qes1_13 {
 		System.out.println("体重は"+e+"kgです");
 		str = "オムライス";
 		System.out.println("好きな食べ物は"+str+"です");
-		System.out.println("BMIは"+e/(f*f)*10000+"です");
+		double bm1 = e / (f*f) * 10000;
+		String bmi1 = String.format("%.1f", bm1);
+		System.out.println("BMIは" + bmi1 + "です");
 		
 		/*
-		 * Qes9
+		 * Qes9 8で使用した変数【年齢・身長・体重】の数値を和算で自己代入し、下記の通りコンソールに出力してください
 		 */
 		
 		//コンソールに出力
@@ -138,10 +154,12 @@ public class Qes1_13 {
 		System.out.println("体重は"+e+"kgです");
 		str = "オムライス";
 		System.out.println("好きな食べ物は"+str+"です");
-		System.out.println("BMIは"+e/(f*f)*10000+"です");
+		double bm2 = e / (f*f) * 10000;
+		String bmi2 = String.format("%.1f", bm2);
+		System.out.println("BMIは" + bmi2 + "です");
 		
 		/*
-		 * Qes10
+		 * Qes10 8で使用した年齢が25歳以上ならtrueが出力されるようにしてください。ただしif文は使いません
 		 */
 		
 		//8で使用した年齢が25歳以上ならtrueが出力されるようにしてください。ただしif文は使いません
@@ -150,7 +168,7 @@ public class Qes1_13 {
 		System.out.println(num > 25);
 		
 		/*
-		 * Qes11
+		 * Qes11 8で使用した【年齢・身長・体重】を文字列型に型変換し繋げて出力してください
 		 */
 		
 		//8で使用した【年齢・身長・体重】を文字列型に型変換し繋げて出力してください
@@ -161,22 +179,22 @@ public class Qes1_13 {
 		String str3 = Float.toString(e);
 		String str4 = String.join(str1, str2, str3);
 		System.out.println(str4);
-		System.out.println(str2);
 		
 		
 		/*
-		 * Qes12
+		 * Qes12 11で変換した【年齢・身長】を整数型に変換して出力してください
 		 */
 		
 		//11で変換した【年齢・身長】を整数型に変換して出力してください
 		int num1 = Integer.parseInt(str1);
-		int num2 = (int)168.5;
-		System.out.println("年齢=" + num1);
-		System.out.println("身長=" + num2);
+		int num2 = (int)f;
+		System.out.println("年齢=" + num1 + "歳");
+		System.out.println("身長=" + num2 + "cm");
 		
 		
 		/*
-		 * Qes13
+		 * Qes13 12で変換した【年齢・身長】で【年齢が25もしくは身長が160以上】であればtrueを出力してください
+		 * ただしif文は使わないでください
 		 */
 		
 		//12で変換した【年齢・身長】で【年齢が25もしくは身長が160以上】であればtrueを出力してください
