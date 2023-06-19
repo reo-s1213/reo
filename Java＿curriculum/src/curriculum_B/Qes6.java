67e9a85e5c8c312f7cb143d08f4644b9668570ff
reo-s1213 <133978237+reo-s1213@users.noreply.github.com>
2023-06-19 15:03:03 +0900

    コメントを追加

--- Java＿curriculum/src/curriculum_B/Qes6.java
+++ Java＿curriculum/src/curriculum_B/Qes6.java
@@ -21,18 +21,23 @@ public class Qes6 {
 		 */
 	
 
-                //
+                // スキャナーインスタンスを作成する
 		        Scanner scanner = new Scanner( System.in );
+		        // ランダムインスタンスを作成する
 		        Random random = new Random();
 
+		        // ユーザーからの入力を受け取る
 		        String input = scanner.nextLine();
+		        // 入力を「、」で区切る
 		        String[] products = input.split( "、" );
 
+		        // 商品ごとに処理を行う
 		        for ( String product : products ) {
 		            int remainingCount;
 		            switch ( product ) {
 		                case "テレビ":
 		                case "ディスプレイ":
+		                	// 0から11までのランダムの値を生成する
 		                    int randomDisplay = random.nextInt( 12 );
 		                    remainingCount = product.equals( "テレビ" ) || product.equals( "ディスプレイ" ) ? randomDisplay : 11 - randomDisplay;
 		                    System.out.println( product + "の残り台数は" + remainingCount + "台です" );
@@ -42,10 +47,13 @@ public class Qes6 {
 		                case "扇風機":
 		                case "洗濯機":
 		                case "加湿器":
+		                	// 0から11までのランダムの値を生成する
 		                    remainingCount = random.nextInt( 12 );
+		                    // 残り台数を入力する
 		                    System.out.println( product + "の残り台数は" + remainingCount + "台です" );
 		                    break;
 		                default:
+		                	// して以外の商品について出力
 		                    System.out.println( "『" + product + "』は指定の商品ではありません" );
 		                    break;
 		            }
--- Java＿curriculum/src/curriculum_B/Qes7.java
+++ Java＿curriculum/src/curriculum_B/Qes7.java
@@ -16,11 +16,13 @@ public class Qes7 {
 		
 
 		
+		        // スキャナーインスタンスを作成
 		        Scanner scanner = new Scanner(System.in);
 
 		        System.out.print("生徒の人数を入力してください（2以上）: ");
+		        // 生徒人数を入力する
 		        int numberOfStudents = scanner.nextInt();
-		     // 改行文字を消費
+		        // 改行文字を消費
 		        scanner.nextLine(); 
 
 		        int[] englishScores = new int[numberOfStudents];
@@ -28,58 +30,81 @@ public class Qes7 {
 		        int[] scienceScores = new int[numberOfStudents];
 		        int[] socialScores = new int[numberOfStudents];
 
-		        for (int i = 0; i < numberOfStudents; i++) {
-		            System.out.print((i + 1) + "人目の『英語』の点数を入力してください: ");
+		        for ( int i = 0; i < numberOfStudents; i++ ) {
+		            System.out.print( ( i + 1 ) + "人目の『英語』の点数を入力してください: " );
+		            // 英語の点数を入力する
 		            englishScores[i] = scanner.nextInt();
-		            System.out.print((i + 1) + "人目の『数学』の点数を入力してください: ");
+		            System.out.print( ( i + 1 ) + "人目の『数学』の点数を入力してください: " );
+		            // 数学の点数を入力する
 		            mathScores[i] = scanner.nextInt();
-		            System.out.print((i + 1) + "人目の『理科』の点数を入力してください: ");
+		            System.out.print( ( i + 1 ) + "人目の『理科』の点数を入力してください: " );
+		            // 理科のの点数を入力する
 		            scienceScores[i] = scanner.nextInt();
-		            System.out.print((i + 1) + "人目の『社会』の点数を入力してください: ");
+		            System.out.print( ( i + 1 ) + "人目の『社会』の点数を入力してください: " );
+		            // 社会の点数を入力する
 		            socialScores[i] = scanner.nextInt();
 
-		         // 改行文字を消費
+		            // 改行文字を消費
 		            scanner.nextLine(); 
 		        }
 
 		        scanner.close();
 
-		        calculateAverages(englishScores, mathScores, scienceScores, socialScores);
-		        calculateSubjectAverages(englishScores, mathScores, scienceScores, socialScores);
+		        // 各生徒、各教科の平均点数を計算して出力する
+		        calculateAverages( englishScores, mathScores, scienceScores, socialScores );
+		        // 全体の科目平均点と各教科の平均点を計算して出力する
+		        calculateSubjectAverages( englishScores, mathScores, scienceScores, socialScores );
 		    }
-
-		    private static void calculateAverages(int[] englishScores, int[] mathScores, int[] scienceScores, int[] socialScores) {
+	
+	        // 各生徒の科目平均点を計算して出力するメソッド
+		    private static void calculateAverages( int[] englishScores, int[] mathScores, int[] scienceScores, int[] socialScores ) {
 		        int numberOfStudents = englishScores.length;
 
-		        for (int i = 0; i < numberOfStudents; i++) {
-		            double average = (englishScores[i] + mathScores[i] + scienceScores[i] + socialScores[i]) / 4.0;
-		            System.out.printf("%d人目の平均点は%.2f点です。\n", (i + 1), average);
+		        for ( int i = 0; i < numberOfStudents; i++ ) {
+		        	// 寡黙の平均点を計算する
+		            double average = ( englishScores[i] + mathScores[i] + scienceScores[i] + socialScores[i] ) / 4.0;
+		            // 平均点を出力する
+		            System.out.printf( "%d人目の平均点は%.2f点です。\n", ( i + 1 ), average );
 		        }
 		    }
 
-		    private static void calculateSubjectAverages(int[] englishScores, int[] mathScores, int[] scienceScores, int[] socialScores) {
+		    // 全体の科目平均点と各教科の平均点を計算して出力するメソッド
+		    private static void calculateSubjectAverages( int[] englishScores, int[] mathScores, int[] scienceScores, int[] socialScores ) {
 		        int numberOfStudents = englishScores.length;
-		        double englishAverage = calculateAverage(englishScores, numberOfStudents);
-		        double mathAverage = calculateAverage(mathScores, numberOfStudents);
-		        double scienceAverage = calculateAverage(scienceScores, numberOfStudents);
-		        double socialAverage = calculateAverage(socialScores, numberOfStudents);
-		        double overallAverage = (englishAverage + mathAverage + scienceAverage + socialAverage) / 4.0;
+		        // 英語の平均点を計算する
+		        double englishAverage = calculateAverage( englishScores, numberOfStudents );
+		        // 数学の平均点を計算する
+		        double mathAverage = calculateAverage( mathScores, numberOfStudents );
+		        // 理科の平均点を計算する
+		        double scienceAverage = calculateAverage( scienceScores, numberOfStudents );
+		        // 社会の平均点を計算する
+		        double socialAverage = calculateAverage( socialScores, numberOfStudents );
+		        // 残体の平均点を計算する
+		        double overallAverage = ( englishAverage + mathAverage + scienceAverage + socialAverage ) / 4.0;
 
-		        System.out.printf("英語の平均点は%.2f点です。\n", englishAverage);
-		        System.out.printf("数学の平均点は%.2f点です。\n", mathAverage);
-		        System.out.printf("理科の平均点は%.2f点です。\n", scienceAverage);
-		        System.out.printf("社会の平均点は%.2f点です。\n", socialAverage);
-		        System.out.printf("全体の平均点は%.2f点です。\n", overallAverage);
+		        // 英語の平均点を出力する
+		        System.out.printf( "英語の平均点は%.2f点です。\n", englishAverage );
+		        // 数学の平均点を出力する
+		        System.out.printf( "数学の平均点は%.2f点です。\n", mathAverage );
+		        // 理科の平均点を出力する
+		        System.out.printf( "理科の平均点は%.2f点です。\n", scienceAverage );
+		        // 社会の平均点を出力する
+		        System.out.printf( "社会の平均点は%.2f点です。\n", socialAverage );
+		        // 全体の平均点を出力する
+		        System.out.printf( "全体の平均点は%.2f点です。\n", overallAverage );
 		    }
 
-		    private static double calculateAverage(int[] scores, int numberOfStudents) {
+		   // 配列の平均値を計算するメソッド
+		    private static double calculateAverage( int[] scores, int numberOfStudents ) {
 		        int sum = 0;
 
-		        for (int i = 0; i < numberOfStudents; i++) {
+		        for ( int i = 0; i < numberOfStudents; i++ ) {
+		        	// 点数を合計する
 		            sum += scores[i];
 		        }
 
-		        return (double) sum / numberOfStudents;
+		        // 平均点を計算して返す
+		        return ( double ) sum / numberOfStudents;
 		    }
 		
 
