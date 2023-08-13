61f8e81778d8e4f708728f0188fc6b6277334fe8
reo-s1213 <133978237+reo-s1213@users.noreply.github.com>
2023-08-13 17:32:35 +0900

    完成

---
+++ Java1.20_1.22.1/src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import processing.Processing;
+
+public class Main {
+	public static void main(String[] args) {
+	
+		Processing processing = new Processing();
+		
+		processing.printMessage();
+	}
+
+}
