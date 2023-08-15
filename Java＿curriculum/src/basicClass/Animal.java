a2c4567459dda7d1239d4a5975a762fa4db09ce4
reo-s1213 <133978237+reo-s1213@users.noreply.github.com>
2023-08-15 23:34:25 +0900

    修正

--- Java＿curriculum/src/basicClass/Animal.java
+++ Java＿curriculum/src/basicClass/Animal.java
@@ -8,7 +8,7 @@ public class Animal {
 	public static void main(String[] args) {
 
 		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
-		Dog dog = new Dog();
+		Dog dog = new Dog("太郎", 2);
 		System.out.println("動物の名前:" + dog.name);
 
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
