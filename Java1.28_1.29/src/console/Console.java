b94f4101ce8e451f12e3c31e4bc32d877f33e442
reo-s1213 <133978237+reo-s1213@users.noreply.github.com>
2023-10-03 08:28:02 +0900

    修正

--- Java1.28_1.29/src/console/Console.java
+++ Java1.28_1.29/src/console/Console.java
@@ -8,56 +8,67 @@ import java.util.Scanner;
 import prefecture.Prefecture;
 
 public class Console {
-	public static void main(String[] args) {
-		List<Prefecture> prefectures = new ArrayList<>();
+    public static void main(String[] args) {
+        List<Prefecture> prefectures = new ArrayList<>();
 
-		// 都道府県データの初期化
-		prefectures.add(new Prefecture("北海道:札幌市:83424"));
-		prefectures.add(new Prefecture("青森県:青森市:9646"));
-		prefectures.add(new Prefecture("岩手県:盛岡市:15275"));
-		prefectures.add(new Prefecture("宮城県:仙台市:7282"));
-		prefectures.add(new Prefecture("秋田県:秋田市:11638"));
-		prefectures.add(new Prefecture("山形県:山形市:9323"));
-		prefectures.add(new Prefecture("福島県:福島市:13784"));
-		prefectures.add(new Prefecture("茨城県:水戸市:6097"));
-		prefectures.add(new Prefecture("栃木県:宇都宮市:6408"));
-		prefectures.add(new Prefecture("群馬県:前橋市:6362"));
-		prefectures.add(new Prefecture("埼玉県:さいたま市:3798"));
+        // 都道府県データの初期化
+        prefectures.add(new Prefecture("北海道:札幌市:83424"));
+        prefectures.add(new Prefecture("青森県:青森市:9646"));
+        prefectures.add(new Prefecture("岩手県:盛岡市:15275"));
+        prefectures.add(new Prefecture("宮城県:仙台市:7282"));
+        prefectures.add(new Prefecture("秋田県:秋田市:11638"));
+        prefectures.add(new Prefecture("山形県:山形市:9323"));
+        prefectures.add(new Prefecture("福島県:福島市:13784"));
+        prefectures.add(new Prefecture("茨城県:水戸市:6097"));
+        prefectures.add(new Prefecture("栃木県:宇都宮市:6408"));
+        prefectures.add(new Prefecture("群馬県:前橋市:6362"));
+        prefectures.add(new Prefecture("埼玉県:さいたま市:3798"));
 
-		Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-		System.out.println("都道府県の番号を1~10の中でカンマ区切りで入力してください：");
-		String input = scanner.nextLine();
-		String[] inputArray = input.split(",");
+        List<Integer> selectedNumbers = new ArrayList<>();
 
-		List<Integer> selectedNumbers = new ArrayList<>();
-		for (String inputItem : inputArray) {
-			int number = Integer.parseInt(inputItem);
-			if (number > 0 && number <= prefectures.size()) {
-				// 配列のインデックスに合わせて調整
-				selectedNumbers.add(number - 1);
-			}
-		}
+        while (selectedNumbers.isEmpty()) {
+            System.out.println("都道府県の番号を1~10の中でカンマ区切りで入力してください：");
+            String input = scanner.nextLine();
+            String[] inputArray = input.split(",");
 
-		System.out.println("ソート方法を選択してください（ascまたはdesc）：");
-		String sortOption = scanner.nextLine();
+            for (String inputItem : inputArray) {
+                try {
+                    int number = Integer.parseInt(inputItem);
+                    if (number > 0 && number <= prefectures.size()) {
+                        selectedNumbers.add(number - 1); // 配列のインデックスに合わせて調整
+                    } else {
+                        System.out.println("無効な番号が入力されました。1から10の番号を入力してください。");
+                        selectedNumbers.clear();
+                        break;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("無効な番号が入力されました。1から10の番号を入力してください。");
+                    selectedNumbers.clear();
+                    break;
+                }
+            }
+        }
 
-		if ("asc".equalsIgnoreCase(sortOption)) {
-			Collections.sort(selectedNumbers);
-		} else if ("desc".equalsIgnoreCase(sortOption)) {
-			Collections.sort(selectedNumbers, Collections.reverseOrder());
-		} else {
-			System.out.println("無効なソート方法です。昇順か降順を選択してください。");
-			// スキャナーをクローズ
-			scanner.close();
-			return;
-		}
+        System.out.println("ソート方法を選択してください（ascまたはdesc）：");
+        String sortOption = scanner.nextLine();
 
-		for (int number : selectedNumbers) {
-			Prefecture selectedPrefecture = prefectures.get(number);
-			System.out.println(selectedPrefecture.toString());
-		}
-		// スキャナーをクローズ
-		scanner.close();
-	}
+        if ("asc".equalsIgnoreCase(sortOption)) {
+            Collections.sort(selectedNumbers);
+        } else if ("desc".equalsIgnoreCase(sortOption)) {
+            Collections.sort(selectedNumbers, Collections.reverseOrder());
+        } else {
+            System.out.println("無効なソート方法です。昇順か降順を選択してください。");
+            scanner.close(); // スキャナーをクローズ
+            return;
+        }
+
+        for (int number : selectedNumbers) {
+            Prefecture selectedPrefecture = prefectures.get(number);
+            System.out.println(selectedPrefecture.toString());
+        }
+
+        scanner.close(); // スキャナーをクローズ
+    }
 }
